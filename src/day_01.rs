99bfe30194973236635d8ee5d87b3685a3c3c6ce
Charlton Roberts <charltonroberts@gmail.com>
2022-01-11 01:26:49 +0000

    Refactor day 1 part 2

--- src/day_01.rs
+++ src/day_01.rs
@@ -26,12 +26,12 @@ pub fn part_1(depths: &Vec<String>) -> u32 {
 }
 
 const WINDOW_SIZE: usize = 3;
-fn part_2(depths: &Vec<u32>) -> u32 {
+pub fn part_2(depths: &Vec<String>) -> u32 {
     let mut increases = 0;
     for (i, _) in depths.iter().enumerate() {
 
         // if we're past the final two windows to compare
-        if (i + WINDOW_SIZE + 1) > depths.len() {
+        if (i + WINDOW_SIZE + 1) > (depths.len() - 1) {
             break;
         }
 
@@ -40,7 +40,10 @@ fn part_2(depths: &Vec<u32>) -> u32 {
         let start_idx = i;
         let end_idx = start_idx + WINDOW_SIZE;
         for j in start_idx..end_idx {
-            window_a += depths[j];
+          match depths[j].parse::<u32>() {
+            Ok(depth) => window_a += depth,
+            Err(_) => panic!("Could not parse depth {}", depths[j]),
+          };
         }
 
         // Calculate the second window
@@ -48,7 +51,10 @@ fn part_2(depths: &Vec<u32>) -> u32 {
         let start_idx = i + 1;
         let end_idx = start_idx + WINDOW_SIZE;
         for j in start_idx..end_idx {
-            window_b += depths[j];
+          match depths[j].parse::<u32>() {
+            Ok(depth) => window_b += depth,
+            Err(_) => panic!("Could not parse depth {}", depths[j]),
+          };
         }
 
         // Record windowed depth increases
@@ -58,50 +64,3 @@ fn part_2(depths: &Vec<u32>) -> u32 {
     }
     increases
 }
-
-// async fn get_input() -> Result<Vec<u32>, Error> {
-//     // Load the session cookie variable
-//     let key = ADVENT_CODE_SESSION_COOKIE_ENV_VAR;
-//     let session_cookie = load_env_var(key);
-
-//     // Make the request for the input
-//     let request_url = format!(
-//         "https://adventofcode.com/2021/day/{day}/input",
-//         day = "1",
-//     );
-//     let client = reqwest::Client::new();
-//     let response = client.get(request_url)
-//         .header("cookie", format!("session={}", session_cookie))
-//         .send()
-//         .await?;
-//     let input = response.text().await?;
-
-//     // Get vector of depths
-//     let depths_str: Vec<&str> = input.split("\n").collect();
-//     let mut depths: Vec<u32> = Vec::new();
-//     for depth_str in depths_str.iter() {
-//         match depth_str.parse::<u32>() {
-//             Ok(depth) => depths.push(depth),
-//             Err(_) => {}
-//         };
-//     }
-//     Ok(depths)
-// }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-
-//     // Load environment variables from .env file
-//     dotenv::dotenv().ok();
-
-//     let depths: Vec<u32> = get_input().await?;
-//     println!("Number of depths recorded: {}", depths.len());
-
-//     let increases = part_1(&depths);
-//     println!("Number of depth increases (part 1): {}", increases);
-
-//     let increases2 = part_2(&depths);
-//     println!("Number of depth increases (part 2): {}", increases2);
-
-//     Ok(())
-// }
--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,14 @@ async fn main() -> Result<(), Error>{
   // Load environment variables from .env file
   dotenv::dotenv().ok();
   
+  // Day 1
   let input = lines_to_vec(fetch_input(1).await?);
   let day_1_part_1_answer = day_01::part_1(&input);
+  let day_1_part_2_answer = day_01::part_2(&input);
   println!("Day 1, part 1: Number of increases is {:?}", day_1_part_1_answer);
+  println!("Day 1, part 2: Number of decreases is {:?}", day_1_part_2_answer);
   assert_eq!(day_1_part_1_answer, 1226);
+  assert_eq!(day_1_part_2_answer, 1252);
 
   Ok(())
 }
